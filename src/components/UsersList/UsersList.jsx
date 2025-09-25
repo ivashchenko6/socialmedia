b5c96a29c918f849d5defcf385138c8363ff6483
Kyryl Ivashchenko <love2dance@Kyryls-Laptop.local>
2025-09-25 10:50:53 -0700

    add friend, delete friend design

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,12 +1,12 @@
-import UserItem from "../UserItem/UserItem";
+import UserItem from '../UserItem/UserItem';
 
-import "./usersList.scss";
+import './usersList.scss';
 const UsersList = ({ users }) => {
     const userItem = users.map((user, i) => {
         return <UserItem key={user.id} user={user} />;
     });
 
-    return <ul className="users-list">{userItem}</ul>;
+    return <ul className='users-list'>{userItem}</ul>;
 };
 
 export default UsersList;
