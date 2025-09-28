5171b3271965ee6f3edb6aeb2019b370fe0fe8d3
Kyryl  Ivashchenko <kirill040605@gmail.com>
2025-09-28 09:57:02 -0700

    updated structure to use React Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import UsersList from "../UsersList/UsersList";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "../Pages/MainPage/MainPage";
 import "./app.scss";
 import Header from "../Header/Header";
+
 const App = () => {
     const apiURL = "https://jsonplaceholder.typicode.com/users";
     const [users, setUsers] = useState([]);
@@ -17,7 +19,14 @@ const App = () => {
     return (
         <div className="app">
             <Header/>
-            <UsersList users={users} />
+
+            <Routes>
+                <Route path="/" element={<MainPage users={users}/>} />
+
+            </Routes>
+
+
+
         </div>
     );
 };
